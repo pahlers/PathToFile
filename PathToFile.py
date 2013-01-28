bf23eacdddb0749ef39f8fa71d2e85799544ec32
Eon le Papillon <eonlepappillon@gmail.com>
2013-01-28 09:38:00 +0100

    added improvements
    - doc
    - check if directory exist
    - support languages
    - add absolute path (guesstimate)

--- PathToFile.py
+++ PathToFile.py
@@ -1,18 +1,42 @@
 import sublime_plugin
+import os
 from os import path
+from PathToFileLanguages import PathToFileLanguages
 
 
 class Pathtofile(sublime_plugin.TextCommand):
     def run(self, edit):
-        print 'run PathToFile'
-
+        """
+        __init__ of this plugin
+        """
         view = self.view
-        startPath = path.dirname(view.file_name())
-        sels = view.sel()
+        syntax = self.getsyntax(view)
+        selections = view.sel()
 
-        for sel in sels:
-            newPath = path.normpath(path.join(startPath, view.substr(sel)))
+        for selection in selections:
+            try:
+                new_path = getattr(PathToFileLanguages(), syntax)(view, selection)
+            except AttributeError:
+                new_path = PathToFileLanguages().default(view, selection)
 
-            print 'PathToFile open ' + newPath
+            self.checkdirectory(new_path)
+            view.window().open_file(new_path)
 
-            view.window().open_file(newPath)
+    def getsyntax(self, view):
+        """
+        return the syntax of the current file
+        """
+        syntax = view.settings().get('syntax')
+
+        return path.basename(syntax).split('.', 1)[0].lower()
+
+    def checkdirectory(self, new_path):
+        """
+        check directory
+
+        if doesn't exists
+            make directory
+        """
+        directory = path.dirname(new_path)
+        if(path.exists(directory) == False):
+            os.makedirs(directory)
---
+++ PathToFileLanguages.py
@@ -0,0 +1,37 @@
+from os import path
+
+
+class PathToFileLanguages:
+    """PathToFileLanguages
+       Methods with language specific knowledge of paths
+    """
+
+    def _abspath(self, view, start_path, end_path):
+        """
+        Absolute path (guesstimate)
+        """
+        new_start_path = start_path
+
+        for folder in view.window().folders():
+            if start_path.find(folder) != -1:
+                new_start_path = folder
+
+        return path.normpath(path.join(new_start_path, end_path[1:]))
+
+    def _relpath(self, view, start_path, end_path):
+        """
+        Relative path
+        """
+        return path.normpath(path.join(start_path, end_path))
+
+    def default(self, view, selection):
+        """
+        default path builder
+        """
+        start_path = path.dirname(view.file_name())
+        end_path = view.substr(selection)
+
+        if path.isabs(end_path):
+            return self._abspath(view, start_path, end_path)
+        else:
+            return self._relpath(view, start_path, end_path)
